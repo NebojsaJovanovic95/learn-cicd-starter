6e3a646402cfe1e3719efb9e4e223563a4fe1975
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-02-13 23:03:09 -0500

    got a nice set of tests to run

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,35 +6,61 @@ import (
 )
 
 func TestGetAPIKeys(t *testing.T) {
-	test := struct {
+	tests := []struct {
 		Name          string
 		Header        string
 		ExpectedKey   string
 		ExpectedError bool
 	}{
-		Name:          "valid key",
-		Header:        "ApiKey abc123",
-		ExpectedKey:   "abc123",
-		ExpectedError: false,
+		{
+			Name:          "valid header",
+			Header:        "ApiKey abc123",
+			ExpectedKey:   "abc123",
+			ExpectedError: false,
+		},
+		{
+			Name:          "missing header",
+			Header:        "",
+			ExpectedError: true,
+		},
+		{
+			Name:          "wrong prefix",
+			Header:        "Bearer abc123",
+			ExpectedError: true,
+		},
+		{
+			Name:          "no key provided",
+			Header:        "ApiKey",
+			ExpectedError: true,
+		},
+		{
+			Name:          "extra spaces",
+			Header:        "ApiKey    abc123",
+			ExpectedError: false,
+			ExpectedKey:   "abc123",
+		},
 	}
-	t.Run(test.Name, func(t *testing.T) {
-		headers := http.Header{}
-		if test.Header != "" {
-			headers.Set("Authorization", test.Header)
-		}
+	for _, test := range tests {
 
-		key, err := GetAPIKey(headers)
+		t.Run(test.Name, func(t *testing.T) {
+			headers := http.Header{}
+			if test.Header != "" {
+				headers.Set("Authorization", test.Header)
+			}
 
-		if err == nil && test.ExpectedError {
-			t.Fatal("expected error but got none")
-		}
+			key, err := GetAPIKey(headers)
 
-		if err != nil && !test.ExpectedError {
-			t.Fatal("expected no error but got one")
-		}
+			if err == nil && test.ExpectedError {
+				t.Fatal("expected error but got none")
+			}
 
-		if key != test.ExpectedKey {
-			t.Fatalf("key %q while %q expected", key, test.ExpectedKey)
-		}
-	})
+			if err != nil && !test.ExpectedError {
+				t.Fatal("expected no error but got one")
+			}
+
+			if key != test.ExpectedKey {
+				t.Fatalf("key %q while %q expected", key, test.ExpectedKey)
+			}
+		})
+	}
 }
