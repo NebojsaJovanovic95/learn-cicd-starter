9ecd4cae6de0266ab5a051f9b5eada6d224cb037
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-02-14 12:54:54 -0500

    fixed security issues

--- json.go
+++ json.go
@@ -23,12 +23,10 @@ func respondWithError(w http.ResponseWriter, code int, msg string, logErr error)
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
-	if err != nil {
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
 		return
 	}
-	w.WriteHeader(code)
-	w.Write(dat)
 }
